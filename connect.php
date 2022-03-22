5d1db950c5522c630125121871b8f5d77851fdfb
AakarshitChaurasia <71507490+AakarshitChaurasia@users.noreply.github.com>
2022-03-23 01:00:40 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+
+//class Connect{
+
+//function con(){
+DEFINE("DB_HOST", "andro.mysql.database.azure.com");
+DEFINE("DB_USER", "andro");
+DEFINE("DB_PASS","@Aicte123");
+DEFINE("DB_NAME","android");
+
+$con = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+if(!$con){
+    echo 'could not establish connection';
+}
+//}
+
+//}
+
+// $cone = new Connect();
+ //$cone->con();
+ 
+?>
